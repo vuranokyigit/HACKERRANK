52e58cff41b1aa4d58f9a5cb22931367c0455534
VuranokYigit <fyvuranokk@gmail.com>
2023-03-10 15:52:22 +0300

    Update day3Alternative.js

--- day3Alternative.js
+++ day3Alternative.js
@@ -20,17 +20,10 @@ function readLine() {
 }
 function main() {
     const N = parseInt(readLine().trim(), 10);
-    if (N%2 != 0){
+   if ((N % 2 != 0) || (N >= 6 && N <= 20)) {
         console.log("Weird")
-    }
-    if (N%2 == 0){
-        if(N>=2 && N<=5){
-            console.log("Not Weird");
-        }else if(N>=6 && N<=20){
-            console.log("Weird");
-        }else{
-            console.log("Not Weird");
-        }
+    } else {
+        console.log("Not Weird");
     }
     
 
