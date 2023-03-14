a94b6b1bb71841c55c98bf3ace43bd5f88558949
VuranokYigit <fyvuranokk@gmail.com>
2023-03-14 10:44:33 +0300

    Create day5Alternative.js

---
+++ day5Alternative.js
@@ -0,0 +1,37 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+    let i = 1;
+    while(i<=10){
+        var result = n * i;
+    
+        console.log(n+ " x " +i +" = " +result);
+        i++;
+        
+    }
+        
+
+}
