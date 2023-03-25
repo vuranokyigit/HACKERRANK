b22deb2d410b55f47ffaca6d4939c0657120a5b9
VuranokYigit <fyvuranokk@gmail.com>
2023-03-25 13:42:37 +0300

    Create day9.js

---
+++ day9.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'factorial' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER n as parameter.
+ */
+
+function factorial(n) {
+    // Write your code here
+    if (n<0){
+        console.log("Math Error"); // n cannot be negative
+        return n;
+    }else if(n<=1){
+        return 1;
+    }else{
+        for(let i=n-1; i>=1; i--){
+            n = n * i
+        }
+        return n;
+    }
+    
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const result = factorial(n);
+
+    ws.write(result + '\n');
+
+    ws.end();
+   
+}
