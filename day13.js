de2dc19696dd164102a9ce2acc8a3a0e55b2011b
VuranokYigit <fyvuranokk@gmail.com>
2023-04-07 10:49:12 +0300

    Create day13.js

---
+++ day13.js
@@ -0,0 +1,10 @@
+class MyBook extends Book{
+    constructor(title, author, price){
+        super(title, author);
+        this.price = price;
+    }
+    display(){
+        console.log("Title: " + this.title);
+        console.log("Author: " + this.author);
+        console.log("Price: " + this.price);
+    }
