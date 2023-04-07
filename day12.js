4a1a6ad3158d397a2ba31a61acf9a6b6787abdeb
VuranokYigit <fyvuranokk@gmail.com>
2023-04-07 12:22:35 +0300

    update

--- day12.js
+++ day12.js
@@ -1,30 +1,39 @@
+
+
 class Student extends Person {
-    
-    constructor (firstName, lastName, id, scores){
+    //we declare a class Student and we wanted the inheritance other data from Person class for we use 'extends' method.
+
+    constructor(firstName, lastName, id, scores) {
         super(firstName, lastName, id);
         this.scores = scores;
+    }
+
+    calculate() {
+
+        var sumScores = this.scores.reduce((accScores, num) => {
+
+            accScores += num;
+            return accScores;
+
+        });//the code iterates through each element in the "scores" array,
+        //adds it to an accumulator variable called "accScores",
+        //and then returns the final value of the accumulator after all elements have been processed. 
+        //The resulting summary is stored in the variable "sumScores"
+        var numScores = this.scores.length;
+        var averageScore = sumScores / numScores;
+
+        if (averageScore >= 90) {
+            return "O"
+        } else if (averageScore >= 80) {
+            return "E";
+        } else if (averageScore >= 70) {
+            return "A";
+        } else if (averageScore >= 55) {
+            return "P";
+        } else if (averageScore >= 40) {
+            return "D";
+        } else {
+            return "T";
         }
-calculate(){
-var sum = this.scores.reduce((acc, num) => {
-      acc += num;
-      return acc;
-    });
-    var numScores = this.scores.length;
-    var averageScore = sum / numScores;
-    if(averageScore>=90){
-        return "O"
-    }else if(averageScore>=80){
-        return "E";
-    }else if(averageScore>=70){
-        return "A";
-    }else if(averageScore>=55){
-        return "P";
-    }else if(averageScore>=40){
-        return "D";
-    }else{
-       return "T";
     }
-  
-    }
-  
 }
--- day13.js
+++ day13.js
@@ -1,10 +1,20 @@
+
+
+
+
 class MyBook extends Book{
-    constructor(title, author, price){
-        super(title, author);
-        this.price = price;
+//we declare a class mybook and we wanted the inheritance other data from Book class for we use 'extends' method
+    constructor(title, author, price){//all variable is consist of the constructor
+        super(title, author);//super for inherited declared components
+        this.price = price;//select 'price' component
+
     }
-    display(){
+    //creating the method for the output 
+    display(){ 
+
         console.log("Title: " + this.title);
         console.log("Author: " + this.author);
         console.log("Price: " + this.price);
+
     }
+}
